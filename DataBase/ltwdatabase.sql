30d17ca272747a5173a29e9e0e84a58225a345a1
rssadal <adalberto.tg80@hotmail.com>
2023-03-30 14:10:36 -0300

    Semana 8

---
+++ DataBase/ltwdatabase.sql
@@ -0,0 +1,75 @@
+
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION;
+
+-- Table: Cliente
+DROP TABLE IF EXISTS User2;
+
+CREATE TABLE User2 (
+    username        TEXT    PRIMARY KEY,
+    nome            TEXT    NOT NULL,
+    password2       TEXT    NOT NULL,
+    email           TEXT    NOT NULL,
+    role2           TEXT    NOT NULL
+
+);
+
+-- Table: Artigo
+DROP TABLE IF EXISTS Ticket;
+
+CREATE TABLE Ticket (
+    id INTEGER PRIMARY KEY,
+    department   TEXT    NOT NULL,
+    hashtag      TEXT    NOT NULL,
+    date2        TEXT,
+    description2 TEXT   NOT NULL,
+    status2      TEXT   NOT NULL,
+    user_usarname TEXT,
+    FOREIGN KEY (user_usarname) REFERENCES User2(username)
+);
+
+
+-- Table:  UsernameTicke
+DROP TABLE IF EXISTS  UsernameTicket;
+
+CREATE TABLE  UsernameTicke (
+    user_username     TEXT,
+    ticket_id     INTEGER,
+    FOREIGN KEY (user_usarname) REFERENCES User2(username),
+    FOREIGN KEY (ticket_id) REFERENCES Ticket(id)
+);
+
+-- Table:  Department 
+DROP TABLE IF EXISTS  Department ;
+
+CREATE TABLE  Department  (
+    id INTEGER PRIMARY KEY,
+    nome  TEXT 
+  
+);
+
+-- Table:  FAQ 
+DROP TABLE IF EXISTS  Faq ;
+
+CREATE TABLE  Faq  (
+    id INTEGER PRIMARY KEY,
+    title  TEXT NOT NULL,
+    Descrition  TEXT NOT NULL,
+    date2 TEXT NOT NULL
+);
+
+
+-- Table:  faq_department 
+DROP TABLE IF EXISTS  Faq_department ;
+
+CREATE TABLE   Faq_department   (
+    deparment_id  INTEGER,
+    faq_id  INTEGER,
+    FOREIGN KEY (department_id) REFERENCES Department(id),
+    FOREIGN KEY (faq_id) REFERENCES Faq(id)
+
+);
+
+
+
+COMMIT TRANSACTION;
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Trouble-Tickets-Project
+
+# Project description for the 2023 edition of the Web Languages and Technologies course.
+
+# Objective
+Develop a website to streamline and manage trouble tickets effectively. The system should enable users to submit, track, and resolve tickets promptly and efficiently. Additionally, the website should have intuitive user interfaces and reporting functionalities to provide real-time insights into ticket status and performance metrics.
