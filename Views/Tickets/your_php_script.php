2bd404ae32101478df5b0091f8e8c7385de9d527
diogo00up <up201905166@up.pt>
2023-05-18 23:58:28 +0100

    add new ticket

---
+++ Views/Tickets/your_php_script.php
@@ -0,0 +1,49 @@
+<?php
+// your_php_script.php
+
+if (isset($_POST['ticketText']) && isset($_POST['department'])) {
+    $ticketText = $_POST['ticketText'];
+    $department = $_POST['department'];
+    $dateString = $_POST['dateString'];
+    $date = date('d-m-Y'); // Get the current date
+
+    $db = new SQLite3('tickets.db');
+
+    $result = $db->query('SELECT * FROM Ticket');
+
+    // Iterate through each row
+    while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        $description = $row['description2'];
+        if($description == $ticketText){
+            echo "Ticket aready exists";
+            return;
+        }
+    }
+
+    // Get the number of rows in the ticket table
+    $result = $db->query('SELECT COUNT(*) as count FROM Ticket');
+    $row = $result->fetchArray(SQLITE3_ASSOC);
+    $rowCount = $row['count'];
+
+    // Insert the new row into the tickets table with the specified ID
+    $stmt = $db->prepare('INSERT INTO Ticket (id, department, hashtag, date2, description2, status2, user_username) VALUES (:id, :department, :hashtag, :date2, :description2, :status2, :user_username)');
+    $stmt->bindValue(':id', $rowCount, SQLITE3_INTEGER);
+    $stmt->bindValue(':department', $department, SQLITE3_TEXT);
+    $stmt->bindValue(':hashtag', 'hardcoded_hashtag', SQLITE3_TEXT);
+    $stmt->bindValue(':date2', $date, SQLITE3_TEXT);
+    $stmt->bindValue(':description2', $ticketText, SQLITE3_TEXT);
+    $stmt->bindValue(':status2', 'waiting', SQLITE3_TEXT);
+    $stmt->bindValue(':user_username', 'diogo13350', SQLITE3_TEXT); // Replace 'your_username' with an actual value
+    $stmt->execute();
+
+    $db->close();
+    
+    // Prepare the response
+    $response = 'Received ticket text: ' . $ticketText . ', Department: ' . $department . ', Total rows in ticket table: ' . $rowCount . ', Date: ' . $date;
+
+} else {
+    $response = 'No ticket text provided.';
+}
+
+echo $response;
+?>
