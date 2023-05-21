02e524d40e4e293651e2c6b9ff78a4bd069ecd9d
diogo00up <up201905166@up.pt>
2023-05-21 22:16:21 +0100

    fix db

--- Views/Tickets/update_ticket_status.php
+++ Views/Tickets/update_ticket_status.php
@@ -4,18 +4,21 @@
     $newStatus = $_POST['newStatus'];
 
     // Connect to your SQLite database
-    $db = new SQLite3('tickets.db');
+    session_start();
+    include_once('../Login/connect.php');
+
+    global $db;
 
     // Prepare and execute the SQL query to update the ticket status
-    $sql = "UPDATE Ticket SET status2 = :newStatus WHERE id = :ticketId";
-    $stmt = $db->prepare($sql);
-    $stmt->bindValue(':newStatus', $newStatus, SQLITE3_TEXT);
-    $stmt->bindValue(':ticketId', $ticketId, SQLITE3_INTEGER);
+    //$sql = "UPDATE Ticket SET status2 = :newStatus WHERE id = :ticketId";
+    $stmt = $db->prepare('UPDATE Ticket SET status2 = :newStatus WHERE id = :ticketId');
+    $stmt->bindValue(':ticketId', $ticketId);
+    $stmt->bindValue(':newStatus', $newStatus);
     $result = $stmt->execute();
 
     // Check if the update was successful
     if ($result) {
-        echo "Ticket status updated successfully!";
+        echo "Ticket status updated successfully! " . $ticketId . " " . $newStatus;
     }
     else {
         echo "fail in updating row";
